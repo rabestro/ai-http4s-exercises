06414acc5bf6590b6c1a05608a878123c074ec4e
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-01 18:53:02 +0300

    Initial commit

---
+++ build.sbt
@@ -0,0 +1,21 @@
+ThisBuild / version := "0.1.0-SNAPSHOT"
+
+ThisBuild / scalaVersion := "2.13.16"
+
+lazy val root = (project in file("."))
+  .settings(
+    name := "ai-http4s-exercises"
+  )
+
+val Http4sVersion = "1.0.0-M44"
+val CirceVersion = "0.14.12"
+
+libraryDependencies ++= Seq(
+  "org.http4s" %% "http4s-ember-server" % Http4sVersion,
+  "org.http4s" %% "http4s-circe" % Http4sVersion,
+  "org.http4s" %% "http4s-dsl" % Http4sVersion,
+  "io.circe" %% "circe-generic" % CirceVersion,
+  "org.typelevel" %% "log4cats-slf4j" % "2.7.0",
+  "org.slf4j" % "slf4j-simple" % "2.0.17",
+  "org.scalatest" %% "scalatest" % "3.2.19" % Test
+)
---
+++ src/main/scala/Exercise1.scala
@@ -0,0 +1,28 @@
+import cats.effect.{ExitCode, IO, IOApp}
+import org.http4s._
+import org.http4s.dsl.io._
+import org.http4s.ember.server.EmberServerBuilder
+import org.http4s.implicits._
+import org.typelevel.log4cats.LoggerFactory
+import org.typelevel.log4cats.slf4j.Slf4jFactory
+
+object Exercise1 extends IOApp {
+  implicit val logging: LoggerFactory[IO] = Slf4jFactory.create[IO]
+
+  private val httpRoutes = HttpRoutes.of[IO] {
+    case GET -> Root / "hello" / name =>
+      Ok(s"Hello, $name!")
+
+    case req@POST -> Root / "echo" =>
+      req.as[String].flatMap(body => Ok(body))
+  }
+
+  override def run(args: List[String]): IO[ExitCode] =
+    EmberServerBuilder
+      .default[IO]
+      .withHttpApp(httpRoutes.orNotFound)
+      .build
+      .useForever
+      .as(ExitCode.Success)
+}
+
