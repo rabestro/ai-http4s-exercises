b5b1a8d803cd4b1fb14eb627b09c14eda17b5a12
Jegors Cemisovs <jcemisovs@evolution.com>
2025-04-02 12:05:07 +0300

    add get user endpoint and tests

--- src/main/scala/exercise4/routes/UserRoutes.scala
+++ src/main/scala/exercise4/routes/UserRoutes.scala
@@ -1,33 +1,37 @@
 package exercise4.routes
 
 import cats.effect.IO
-import exercise3.model.Product
+import cats.implicits._
+import exercise4.error.{AppError, UserNotFound}
 import exercise4.service.UserService
 import io.circe.Json
-import org.http4s.HttpRoutes
 import org.http4s.circe.CirceEntityCodec._
+import org.http4s.circe._
 import org.http4s.dsl.io._
+import org.http4s.{HttpRoutes, Response}
 
 import java.util.UUID
-import scala.util.Try
 
 class UserRoutes(userService: UserService[IO]) {
 
   val routes: HttpRoutes[IO] = HttpRoutes.of[IO] {
     case GET -> Root / "users" / userId =>
-      IO.fromEither(Try(UUID.fromString(userId)).toEither)
-        .flatMap(userId => Ok(s"User ID: $userId"))
-        .handleErrorWith(_ => BadRequest("Invalid UUID format"))
-
-    case req@POST -> Root / "products" =>
-      req.as[Product].flatMap { product =>
-        Created(product)
-      }.handleErrorWith { error =>
-        BadRequest(Json.obj("error" -> Json.fromString("Failed to parse product request")))
-      }
-    case GET -> Root / "products" / productId =>
-      IO.fromEither(Try(UUID.fromString(productId)).toEither)
-        .flatMap(id => Ok(Product(id, "Some Product", 30.5)))
-        .handleErrorWith(_ => BadRequest(Json.obj("error" -> Json.fromString("Invalid UUID format"))))
+      parseUUID(userId).flatMap { id =>
+        userService.getUser(id).value.flatMap {
+          case Right(user) => Ok(user)
+          case Left(error) => handleUserError(error)
+        }
+      }.handleErrorWith(handleUUIDError)
   }
+
+  private def parseUUID(userId: String): IO[UUID] =
+    IO.fromEither(Either.catchNonFatal(UUID.fromString(userId)).leftMap(_ => new IllegalArgumentException("Invalid UUID format")))
+
+  private def handleUserError(error: AppError): IO[Response[IO]] = error match {
+    case _: UserNotFound => NotFound(Json.obj("error" -> Json.fromString("User not found")))
+    case _ => InternalServerError(Json.obj("error" -> Json.fromString("Internal server error")))
+  }
+
+  private def handleUUIDError(error: Throwable): IO[Response[IO]] =
+    BadRequest(Json.obj("error" -> Json.fromString("Invalid UUID format")))
 }
